7b248060bbc39651a22070912ddaf0f909eae43b
jacky <jaykishan.solanki@beelinesoftwares.com>
2024-01-09 17:01:34 +0530

    Intital Code Commit For Release/production

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, From Main Branch"
+    return "Hello, From Release/production Branch"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
